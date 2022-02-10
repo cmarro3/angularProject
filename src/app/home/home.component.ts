d4ef3ef067f82300b40aee851bd3dfbc1c0f051f
cmarro3 <christophermarroquin352@gmail.com>
2022-02-10 12:53:26 -0600

    last time I'll update. Will create another project in case I'm stuck on something.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,14 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
-
-
-//Ended Angular tutorial at 1:41:22 link: https://www.youtube.com/watch?v=k5E2AVpwsko
-// to run app just do ng serve
